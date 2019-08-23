fa9eb070df64822283101d7dcdcd11c0cd241a57
Krzysztof Faldzinski <kfaldzinski@icloud.com>
2019-08-23 17:33:53 +0200

    Add var: computerMove, playerMove

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');

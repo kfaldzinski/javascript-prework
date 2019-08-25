2b624feae15976dffc670ca43a1b48f440c5e4c3
Krzysztof Faldzinski <kfaldzinski@icloud.com>
2019-08-26 00:08:11 +0200

    Add random selection

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
 }
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+else if (randomNumber == '2') {
+  computerMove = 'papier';
+else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+printMessage('Mój ruch: ' + computerMove);
 }
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');

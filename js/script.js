b4c3d818ac63a4ba7b14b44f2a83aacf1543f141
Krzysztof Faldzinski <kfaldzinski@icloud.com>
2019-08-27 13:00:40 +0200

    Add function.js file

--- js/script.js
+++ js/script.js
@@ -3,12 +3,26 @@ randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
 } else {
   computerMove = 'nieznany ruch';
-}
-else if (randomNumber == '2') {
-  computerMove = 'papier';
-else if (randomNumber == '3') {
-  computerMove = 'nożyce';
+} 
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+	playerMove = 'kamień';
+} else if (playerInput == '2') {
+	playerMove == 'papier';
+} else if (playerInput == '3') {
+	playerMove == 'nożyce';
+} else {
+	playerMove = 'kamień';
+	printMessage('Błędny wybór: ' + playerInput ', domyślnie wybrano 1. ' + playerMove);
 }
+printMessage('Twój ruch: ' + playerMove);

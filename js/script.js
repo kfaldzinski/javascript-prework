ea87bb10bc9ae4031b1c19b27dd8c811e206be61
Krzysztof Faldzinski <kfaldzinski@icloud.com>
2019-08-28 09:23:37 +0200

    Add amendments function.js file

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
+var moveId, computerMove, playerMove, randomNumber, playerInput;
 
 /**
  * Describe this function...
  */
-function getMoveName(argMoveId) {
-  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-  if (argMoveId == 1) {
+function getMoveName(moveId) {
+  console.log('wywołano funkcję getMoveName z argumentem: ' + moveId);
+  if (moveId == 1) {
     return 'kamień';
-  } else if (argMoveId == 2) {
+  } else if (moveId == 2) {
     return 'papier';
-  } else if (argMoveId == 3) {
+  } else if (moveId == 3) {
     return 'nożyce';
   } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '. Domyślnie wybrano "kamień".');
+    printMessage('Nie znam ruchu o id ' + moveId + '. Domyślnie wybrano "kamień".');
     return 'kamień';
   }
 }
@@ -20,20 +20,20 @@ function getMoveName(argMoveId) {
 /**
  * Describe this function...
  */
-function displayResult(argPlayerMove, argComputerMove) {
-  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+function displayResult(playerMove, computerMove) {
+  console.log('wywołano funkcję displayResults z argumentami: ' + playerMove + ', ' + computerMove);
+  if (playerMove == 'papier' && computerMove == 'kamień') {
     printMessage('Wygrywasz!');
-  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+  } else if (playerMove == 'kamień' && computerMove == 'nożyce') {
     printMessage('Wygrywasz!');
-  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+  } else if (playerMove == 'nożyce' && computerMove == 'papier') {
     printMessage('Wygrywasz!');
-  } else if (argPlayerMove == argComputerMove) {
+  } else if (playerMove == computerMove) {
     printMessage('Remis!');
   } else {
     printMessage('Przegrywasz :(');
   }
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
 }
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('wybór ruchu gracza to: ' + playerInput);
